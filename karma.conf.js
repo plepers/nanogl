d722163bd9fdd89a666b473aa1575987b019ece3
Pierre Lepers <pierre.lepers@gmail.com>
2015-12-28 21:16:58 +0100

    rename karma sl name

--- karma.conf.js
+++ karma.conf.js
@@ -82,7 +82,7 @@ module.exports = function(config) {
     // SL and travis config
     sauceLabs: {
       startConnect: true,
-      testName: 'eagl unit tests'
+      testName: 'nanogl unit tests'
     },
 
     // define SL browsers
