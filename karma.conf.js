8042ecd7a64bc621270d99422a44dc0e145f6bd4
Pierre Lepers <pierre.lepers@makemepulse.com>
2017-02-01 13:04:43 +0100

    test runner option to run WEBGL 1 or 2

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,24 @@
 // Karma configuration
 // Generated on Sat Dec 19 2015 12:50:43 GMT+0100 (CET)
 
+
 module.exports = function(config) {
+  var glversion = 1;
+
+  if( config.webgl_version !== undefined ){
+    glversion = config.webgl_version;
+  }
+
+  
+  var invgrep;
+  if( glversion === 1 ){
+    invgrep = '@WEBGL2';
+  }
+  else {
+    invgrep = '@WEBGL1';
+  }
+
+
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -12,6 +29,14 @@ module.exports = function(config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['browserify', 'mocha'],
 
+    // mocha custom option
+    client: {
+      webgl_version: glversion,
+      mocha:{
+        grep : invgrep,
+        invert:true
+      }
+    },
 
     // list of files / patterns to load in the browser
     files: [
